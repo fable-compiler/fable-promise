cb2d73875a8c772b29f204e168f419f3b27bffa4
Chet Husk <chusk3@gmail.com>
2021-08-10 14:08:02 -0500

    Add sample code to prove thenable interop

--- tests/PromiseTests.fs
+++ tests/PromiseTests.fs
@@ -20,6 +20,22 @@ type DisposableAction(f) =
     interface IDisposable with
         member __.Dispose() = f()
 
+/// this is the definition of a thenable from ts2fable's generation VsCode API
+type [<AllowNullLiteral>] Thenable<'T> =
+    abstract ``then``: ?onfulfilled: ('T -> U2<'TResult, Thenable<'TResult>>) * ?onrejected: (obj option -> U2<'TResult, Thenable<'TResult>>) -> Thenable<'TResult>
+    abstract ``then``: ?onfulfilled: ('T -> U2<'TResult, Thenable<'TResult>>) * ?onrejected: (obj option -> unit) -> Thenable<'TResult>
+
+module Thenable =
+    let toPromise (t: Thenable<'t>): JS.Promise<'t> =  unbox t
+    let ofPromise (p: JS.Promise<'t>): Thenable<'t> = unbox p
+
+type Promise.PromiseBuilder with
+    /// to make a value interop with the promise builder, you have to add an
+    /// overload of the `Source` member to convert from your type to a promise.
+    /// because thenables are trivially convertable, we can just unbox them.
+    member x.Source(t: Thenable<'t>): JS.Promise<'t> = Thenable.toPromise t
+
+
 describe "Promise tests" <| fun _ ->
     it "Simple promise translates without exception" <| fun () ->
         promise { return () }
@@ -371,3 +387,15 @@ describe "Promise tests" <| fun _ ->
         )
 
         delayed.``then``(fun _ -> promiseExecutionCount |> equal 1)
+
+    it "Promise can interop with thenables" <| fun () ->
+        let sampleThenable () =
+            promise {
+                return 1
+            }
+            |> Thenable.ofPromise
+
+        promise {
+            let! initialValue = sampleThenable()
+            initialValue |> equal 1
+        }
